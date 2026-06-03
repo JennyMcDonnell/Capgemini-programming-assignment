44c7be577bdc09ccc616507efd68e5c3fa05d7ac
JennyMcDonnell <Jmcdonnell00@gmail.com>
2026-06-03 13:28:52 -0500

    impliment ability to view past moods

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
@@ -27,7 +26,7 @@ public class Main {
                         moodLog.add(createMoodEntry());
                     }
                     catch (Exception e) {
-                        System.out.println("Exception: " + e.getMessage());
+                        System.out.println("Exception: " + e.getMessage() + "\n");
                     }
                     break;
                 case "2": //view past moods
@@ -65,9 +64,94 @@ public class Main {
         return new MoodEntry(dateToAdd, emotionToAdd, noteToAdd);
     }
 
+
+
     public static void viewPreviousMoods(){
-        for (MoodEntry mood: moodLog){
-            System.out.println(mood.getDate().toString() + mood.getMood() + mood.getNote());
+        System.out.println("""
+                View moods by:
+                1. All
+                2. Date
+                3. Emotion
+                4. Most common emotion""");
+        String userInput = scanner.nextLine();
+        switch (userInput){
+            case "1":
+                if (moodLog.isEmpty()){
+                    System.out.println("No moods recorded. Add a mood and try again.");
+                    break;
+                }
+
+                for (MoodEntry mood: moodLog){
+                    System.out.println();
+                    System.out.println("Date: " + mood.getDate().toString());
+                    System.out.println("Mood: " + mood.getMood().toString());
+                    System.out.println("Note: " + mood.getNote());
+                    System.out.println();
+                }
+                break;
+            case "2":
+                if (moodLog.isEmpty()){
+                    System.out.println("No moods recorded. Add a mood and try again.");
+                    break;
+                }
+
+                System.out.println("Enter Date YYYY-MM-DD:");
+                String inputDate = scanner.nextLine();
+                for (MoodEntry mood: moodLog){
+                    if (mood.getDate().equals(LocalDate.parse(inputDate))) {
+                        System.out.println();
+                        System.out.println("Date: " + mood.getDate().toString());
+                        System.out.println("Mood: " + mood.getMood().toString());
+                        System.out.println("Note: " + mood.getNote());
+                        System.out.println();
+                    }
+                }
+                break;
+            case "3":
+                if (moodLog.isEmpty()){
+                    System.out.println("No moods recorded. Add a mood and try again.");
+                    break;
+                }
+
+                System.out.println("Enter Emotion:");
+                String inputEmotion = scanner.nextLine().toUpperCase();
+                try {
+                    for (MoodEntry mood : moodLog) {
+                        if (mood.getMood().equals(Emotions.valueOf(inputEmotion))) {
+                            System.out.println();
+                            System.out.println("Date: " + mood.getDate().toString());
+                            System.out.println("Mood: " + mood.getMood().toString());
+                            System.out.println("Note: " + mood.getNote());
+                            System.out.println();
+                        }
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Emotion not found. Please try again.");
+                }
+                break;
+            case "4":
+                if (moodLog.isEmpty()){
+                    System.out.println("No moods recorded. Add a mood and try again.");
+                    break;
+                }
+
+                HashMap<Emotions, Integer> frequencyMap = new HashMap<>();
+                for (MoodEntry moodEntry: moodLog){
+                    frequencyMap.put(moodEntry.getMood(), frequencyMap.getOrDefault(moodEntry.getMood(), 0) + 1);
+                }
+
+                Emotions mostCommonEmotion = frequencyMap.entrySet().stream()
+                        .max(Map.Entry.comparingByValue())
+                        .map(Map.Entry::getKey)
+                        .orElse(null);
+
+
+                System.out.println("Your most commonly logged emotion is " + mostCommonEmotion.toString().toLowerCase());
+                System.out.println();
+                break;
+            default:
+                System.out.println("unknown option. Returning to main menu.");
         }
+
     }
 }
